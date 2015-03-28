b9a2930ca406ee7ed6846d51da8f6fb055a7f386
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-03-27 19:11:58 -0700

    committing lots o stuff

---
+++ js/GameController.js
@@ -0,0 +1,33 @@
+// IIFE
+(function(){
+	angular
+		.module('GameApp')
+		.controller('GameController', GameController);
+
+		function GameController(){
+
+			var self = this;
+			self.BoxList = [ // An array of objects that are boxes 1 - 9
+
+			// Start of the box arrays
+			{ box: 1 
+			},{ box: 2
+			},{ box: 3
+			},{ box: 4
+			},{ box: 5
+			},{ box: 6
+			},{ box: 7
+			},{ box: 8
+			},{ box: 9
+			}// end of the box arrays
+
+
+
+
+
+			]
+
+
+		}
+
+})();
