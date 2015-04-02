bdb96d4a87df8b78fab67aaf799da16e856380c2
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-04-02 16:13:26 -0700

    updatingcontroller

--- js/GameController.js
+++ js/GameController.js
@@ -12,20 +12,23 @@
 				alert("hi");
 		 	 };
 
-			self.let = {status: "X"} ;
+
+			self.getX = function () {
+				value: "X";
+			};
 
 			self.boxList = [ // An array of objects that are boxes 1 - 9
 
 			// Start of the box arrays
-			{ box: 1
-			},{ box: 2
-			},{ box: 3
-			},{ box: 4
-			},{ box: 5
-			},{ box: 6
-			},{ box: 7
-			},{ box: 8
-			},{ box: 9
+			{ box: 1, status: null
+			},{ box: 2, status: null
+			},{ box: 3, status: null
+			},{ box: 4, status: null
+			},{ box: 5, status: null
+			},{ box: 6, status: null
+			},{ box: 7, status: null
+			},{ box: 8, status: null
+			},{ box: 9, status: null
 			}// end of the box arrays
 
 
